2b3d4fc15617895d9b4714c06e35187eebe7d644
M.F.M <mfmfazrin1986@gmail.com>
2025-04-21 00:03:47 +0300

    chore: update GTM ID to GTM-MLK5VRGH
    Update the Google Tag Manager ID in both the HTML file and the GTM initialization script to reflect the new tracking configuration. This ensures consistent tracking across the application.

--- src/lib/gtm.ts
+++ src/lib/gtm.ts
@@ -4,7 +4,7 @@ declare global {
   }
 }
 
-export const GTM_ID = 'GTM-PWTGRCTV';
+export const GTM_ID = 'GTM-MLK5VRGH';
 export const GA_ID = 'G-EQC6PQ0E9Q';
 
 export const initGTM = () => {
@@ -12,13 +12,15 @@ export const initGTM = () => {
     window.dataLayer = window.dataLayer || [];
 
     // Initialize GTM
-    (function(w: { [key: string]: any },d: Document,s: string,l: string,i: string){
-      w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s) as HTMLScriptElement,dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;
-      if (f && f.parentNode) f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer',GTM_ID);
+    (function (w: { [key: string]: any }, d: Document, s: string, l: string, i: string) {
+      w[l] = w[l] || []; w[l].push({
+        'gtm.start':
+          new Date().getTime(), event: 'gtm.js'
+      }); var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s) as HTMLScriptElement, dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+          'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      if (f && f.parentNode) f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', GTM_ID);
 
     // Initialize GA4
     const script = document.createElement('script') as HTMLScriptElement;
@@ -33,4 +35,4 @@ export const initGTM = () => {
     gtag('js', new Date());
     gtag('config', GA_ID);
   }
-}; 
+};
